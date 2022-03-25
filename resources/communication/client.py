8828a4063b98054a069da203743674dc32a1e996
AutumnSpark1226 <autumnspark1226@gmail.com>
2022-03-25 15:44:26 +0100

    2022.3.25 - encrypted communication (RSA, AES)

--- resources/communication/client.py
+++ resources/communication/client.py
@@ -1,12 +1,54 @@
+import hashlib
+import os
 import socket
 
+from Crypto.Cipher import AES
+from Cryptodome.Cipher import PKCS1_OAEP
+from Cryptodome.PublicKey import RSA
+from Cryptodome.Util import Padding
+
 client_socket = None
+_aes_key = None
+_iv = None
 
 
 def connect(host, port):
     global client_socket
     client_socket = socket.socket()
     client_socket.connect((host, port))
+    # receive public_key
+    public_key = client_socket.recv(1024)
+    # generate _aes_key
+    global _aes_key, _iv
+    _aes_key = os.urandom(32)
+    # generate _iv
+    _iv = os.urandom(16)
+    # create cipher
+    cipher = AES.new(_aes_key, AES.MODE_CBC, _iv)
+    # encrypt aes_key
+    rsa_public_key = RSA.importKey(public_key)
+    oaep_cipher = PKCS1_OAEP.new(rsa_public_key)
+    encrypted_aes_key = oaep_cipher.encrypt(_aes_key)
+    # send encrypted_aes_key
+    client_socket.send(encrypted_aes_key)
+    # encrypt iv
+    encrypted_iv = oaep_cipher.encrypt(_iv)
+    # send encrypted_iv
+    client_socket.send(encrypted_iv)
+    # receive encrypted_hashed_public_key
+    encrypted_hashed_public_key = client_socket.recv(1024)
+    # decrypt hashed_public_key
+    hashed_public_key = cipher.decrypt(encrypted_hashed_public_key).decode()
+    # hash public key
+    hashed_public_key2 = hashlib.sha512(public_key).hexdigest()
+    # encrypt hashed_public_key2
+    encrypted_hashed_public_key2 = cipher.encrypt(hashed_public_key2)
+    # send encrypted_hashed_public_key2
+    client_socket.send(encrypted_hashed_public_key2)
+    # check if hashed_public_key and hashed_public_key2 are equal (they should be)
+    if hashed_public_key2 != hashed_public_key:
+        client_socket.close()
+        raise Exception("encryption or server error")
 
 
 def disconnect():
@@ -14,11 +56,16 @@ def disconnect():
 
 
 def send_text(text):
-    client_socket.send(text.encode())
+    padded_text = Padding.pad(text.encode(), 16)
+    cipher = AES.new(_aes_key, AES.MODE_CBC, _iv)
+    encrypted_text = cipher.encrypt(padded_text)
+    client_socket.send(encrypted_text)
 
 
 def receive_text(size=1024):
-    text = client_socket.recv(size).decode()
+    cipher = AES.new(_aes_key, AES.MODE_CBC, _iv)
+    encrypted_text = client_socket.recv(size)
+    text = str(Padding.unpad(cipher.decrypt(encrypted_text), 16).decode())
     return text
 
 
--- resources/communication/server.py
+++ resources/communication/server.py
@@ -1,4 +1,10 @@
+import hashlib
 import socket
+
+import Cryptodome.Cipher.PKCS1_OAEP
+from Crypto.Cipher import AES
+from Cryptodome.PublicKey import RSA
+from Cryptodome.Util import Padding
 
 server_socket = None
 
@@ -17,16 +23,56 @@ def stop():
 
 def accept_client():
     connection, client_address = server_socket.accept()
-    return connection, client_address
+    # generate rsa key
+    key = RSA.generate(2048)
+    # save private_key
+    private_key = key.exportKey('PEM')
+    rsa_private_key = RSA.importKey(private_key)
+    oaep_cipher = Cryptodome.Cipher.PKCS1_OAEP.new(rsa_private_key)
+    # save public_key
+    public_key = key.publickey().exportKey('PEM')
+    # hash public key
+    hashed_public_key = hashlib.sha512(public_key).hexdigest()
+    # send public_key
+    connection.send(public_key)
+    # receive encrypted aes_key
+    rsa_encrypted = connection.recv(1024)
+    # decrypt aes_key
+    aes_key = oaep_cipher.decrypt(rsa_encrypted)
+    # receive iv
+    rsa_encrypted = connection.recv(1024)
+    # decrypt iv
+    iv = oaep_cipher.decrypt(rsa_encrypted)
+    # create cipher
+    cipher = AES.new(aes_key, AES.MODE_CBC, iv)
+    # encrypt hashed_public_key
+    encrypted_hashed_public_key = cipher.encrypt(hashed_public_key)
+    # send encrypted_hashed_public_key
+    connection.send(encrypted_hashed_public_key)
+    # receive encrypted_hashed_public_key2
+    encrypted_hashed_public_key2 = connection.recv(1024)
+    # decrypt hashed_public_key2
+    hashed_public_key2 = cipher.decrypt(encrypted_hashed_public_key2).decode()
+    # check if hashed_public_key and hashed_public_key2 are equal (they should be)
+    if hashed_public_key2 == hashed_public_key:
+        secure_connection = tuple((connection, aes_key, iv))
+        return secure_connection, client_address
+    else:
+        connection.close()
+        raise Exception("encryption or client error")
 
 
-def send_text(connection, text):
-    connection.send(text.encode())
+def send_text(secure_connection, text):
+    padded_text = Padding.pad(text.encode(), 16)
+    cipher = AES.new(secure_connection[1], AES.MODE_CBC, secure_connection[2])
+    encrypted_text = cipher.encrypt(padded_text)
+    secure_connection[0].send(encrypted_text)
 
 
-def receive_text(connection, size=1024):
-    data = connection.recv(size).decode()
-    text = str(data)
+def receive_text(secure_connection, size=1024):
+    cipher = AES.new(secure_connection[1], AES.MODE_CBC, secure_connection[2])
+    encrypted_text = secure_connection[0].recv(size)
+    text = str(Padding.unpad(cipher.decrypt(encrypted_text), 16).decode())
     return text
 
 
@@ -40,5 +86,5 @@ if __name__ == '__main__':
             break
         print("received: " + test_message)
         send_text(conn, test_message)
-    conn.close()
+    conn[0].close()
     stop()
