79e64021f64686c9484e206f9a97f5ccb21cd4b7
AutumnSpark1226 <autumnspark1226@gmail.com>
2022-05-07 14:16:07 +0200

    2022.5.7 - performance test improvements

--- performance_test.sh
+++ performance_test.sh
@@ -2,19 +2,19 @@
 # test performance
 
 # pybricks-micropython
-START0=$(date +%s)
+START0=$(date +%N)
 pybricks-micropython test.py > /dev/null
-STOP0=$(date +%s)
+STOP0=$(date +%N)
 
 # python3
-START1=$(date +%s)
+START1=$(date +%N)
 python3 test.py > /dev/null
-STOP1=$(date +%s)
+STOP1=$(date +%N)
 
 # calculate time
 TIME0=$((STOP0 - START0))
 TIME1=$((STOP1 - START1))
 
 # print results
-echo "pybricks-micropython: $TIME0 s"
-echo "python3: $TIME1 s"
+echo "pybricks-micropython: $TIME0 ns"
+echo "python3: $TIME1 ns"
