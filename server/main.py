faecc1d08aa156006002d691ae00bb96914aafb0
AutumnSpark1226 <autumnspark1226@gmail.com>
2022-05-28 19:12:01 +0200

    2022.05.28.5

--- server/main.py
+++ server/main.py
@@ -22,19 +22,19 @@ class EV3Connect(threading.Thread):
         # save connections of the robots to the variables (They are separated by their hostnames.)
         server.start(6666)
         print('[server/main.py] server started')
-        # count = 0  # test purposes only
+        count = 0  # test purposes only
         while True:
             con, address = server.accept_client()
             hostname = socket.gethostbyaddr(address[0])[0]  # get hostname
             print('[server/main.py] connection request (' + hostname + ')')
-            if hostname == 'mapping0':
-                # if count == 0: # test purposes only
-                # count = 1 # test purposes only
+            #if hostname == 'mapping0':
+            if count == 0: # test purposes only
+                count = 1 # test purposes only
                 global mapping0_connection
                 mapping0_connection = con
                 print('[server/main.py] ' + hostname + ' connected')
-            elif hostname == 'mapping1':
-                # elif count == 1: # test purposes only
+            #elif hostname == 'mapping1':
+            elif count == 1: # test purposes only
                 global mapping1_connection
                 mapping1_connection = con
                 print('[server/main.py] ' + hostname + ' connected')
@@ -45,7 +45,7 @@ class EV3Connect(threading.Thread):
 
     def stop(self):
         if self.is_alive():
-            os.system('kill ' + str(self.native_id))
+            os.system('kill ' + str(self.native_id))  # easiest way to stop the thread
 
 
 def wait_for_connections():
@@ -84,11 +84,11 @@ def start():
 
 
 def stop():
-    ev3_connect_thread.stop()
     server.send_text(mapping0_connection, 'exit')
     server.send_text(mapping1_connection, 'exit')
     server.stop()
     database.disconnect()
+    ev3_connect_thread.stop()
 
 
 def run():
