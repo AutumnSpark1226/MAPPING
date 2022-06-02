4a1b9e93e7583a0561dc088ddca729336b05bbc3
Benutzer00073 <oskito2007@gmail.com>
2022-06-02 15:06:25 +0200

    2022.26.02.1

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ ev3.speaker.beep()
 test_motor.run_time(500, 2000)
 # Play another beep sound.
 ev3.speaker.beep(1000, 500)
-ev3.speaker.set_volume(100)
+ev3.speaker.set_volume(1000)
 ev3.speaker.say("Jeremias: failure")
 # measure distance
 ultrasonic = UltrasonicSensor(Port.S1)
