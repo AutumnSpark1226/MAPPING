238b184f2655cca0d0937b73a8641fece8fe0b22
AutumnSpark1226 <autumnspark1226@gmail.com>
2022-06-02 15:12:36 +0200

    2022.06.02.2

--- test.py
+++ test.py
@@ -5,17 +5,23 @@ from pybricks.ev3devices import UltrasonicSensor
 from pybricks.hubs import EV3Brick
 from pybricks.parameters import Port
 
-# Initialize the EV3 brick.
-ev3 = EV3Brick()
-# Initialize a motor at port A.
-test_motor = Motor(Port.A)
-# Play a sound.
-ev3.speaker.beep()
-test_motor.run_time(500, 2000)
-# Play another beep sound.
-ev3.speaker.beep(1000, 500)
-ev3.speaker.set_volume(1000)
-ev3.speaker.say("Jeremias: failure")
-# measure distance
-ultrasonic = UltrasonicSensor(Port.S1)
-print("distance: " + str(ultrasonic.distance()))
+
+def test1():
+    # Initialize the EV3 brick.
+    ev3 = EV3Brick()
+    # Initialize a motor at port A.
+    test_motor = Motor(Port.A)
+    # Play a sound.
+    ev3.speaker.beep()
+    test_motor.run_time(500, 2000)
+    # Play another beep sound.
+    ev3.speaker.beep(1000, 500)
+    ev3.speaker.set_volume(1000)
+    ev3.speaker.say("Jeremias: failure")
+    # measure distance
+    ultrasonic = UltrasonicSensor(Port.S1)
+    print("distance: " + str(ultrasonic.distance()))
+
+
+if __name__ == '__main__':
+    test1()
