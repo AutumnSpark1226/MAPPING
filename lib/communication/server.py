2bf10cafd1a32ee97b33cdceb2ea2c9567593f81
AutumnSpark1226 <autumnspark1226@gmail.com>
2022-10-07 14:49:19 +0200

    2022.10.07 - removed encryption

--- lib/communication/server.py
+++ lib/communication/server.py
@@ -27,61 +27,19 @@ def accept_client():
     if not _server_socket:
         raise Exception("server not running")
     connection, client_address = _server_socket.accept()
-    # generate rsa key
-    key = RSA.generate(2048)
-    # save private_key
-    private_key = key.exportKey('PEM')
-    rsa_private_key = RSA.importKey(private_key)
-    oaep_cipher = Cryptodome.Cipher.PKCS1_OAEP.new(rsa_private_key)
-    # save public_key
-    public_key = key.publickey().exportKey('PEM')
-    # hash public key
-    hashed_public_key = hashlib.sha512(public_key).hexdigest().encode()
-    # send public_key
-    connection.send(public_key)
-    # receive encrypted aes_key
-    rsa_encrypted = connection.recv(1024)
-    # decrypt aes_key
-    aes_key = oaep_cipher.decrypt(rsa_encrypted)
-    # receive iv
-    rsa_encrypted = connection.recv(1024)
-    # decrypt iv
-    iv = oaep_cipher.decrypt(rsa_encrypted)
-    # create cipher
-    cipher = AES.new(aes_key, AES.MODE_CBC, iv)
-    # encrypt hashed_public_key
-    encrypted_hashed_public_key = cipher.encrypt(hashed_public_key)
-    # send encrypted_hashed_public_key
-    connection.send(encrypted_hashed_public_key)
-    # receive encrypted_hashed_public_key2
-    encrypted_hashed_public_key2 = connection.recv(1024)
-    # decrypt hashed_public_key2
-    cipher = AES.new(aes_key, AES.MODE_CBC, iv)
-    hashed_public_key2 = cipher.decrypt(encrypted_hashed_public_key2).decode()
-    # check if hashed_public_key and hashed_public_key2 are equal (they should be)
-    if hashed_public_key2 == hashed_public_key.decode():
-        secure_connection = tuple((connection, aes_key, iv))
-        return secure_connection, client_address
-    else:
-        connection.close()
-        raise Exception("encryption or client error")
+    return connection, client_address
 
 
-def send_text(secure_connection, text):
+def send_text(connection, text):
     if not _server_socket:
         raise Exception("server not running")
-    padded_text = Padding.pad(text.encode(), 16)
-    cipher = AES.new(secure_connection[1], AES.MODE_CBC, secure_connection[2])
-    encrypted_text = cipher.encrypt(padded_text)
-    secure_connection[0].send(encrypted_text)
+    connection.send(text.encode())
 
 
-def receive_text(secure_connection, size=1024):
+def receive_text(connection, size=1024):
     if not _server_socket:
         raise Exception("server not running")
-    cipher = AES.new(secure_connection[1], AES.MODE_CBC, secure_connection[2])
-    encrypted_text = secure_connection[0].recv(size)
-    text = str(Padding.unpad(cipher.decrypt(encrypted_text), 16).decode())
+    text = connection.recv(size).decode()
     return text
 
 
