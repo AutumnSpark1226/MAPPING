b09e129c4c7c5afa824f0f38b04d04d6dbed0655
AutumnSpark1226 <autumnspark1226@gmail.com>
2022-12-16 14:41:39 +0100

    2022.12.16.FIX

--- show_error_logs.py
+++ show_error_logs.py
@@ -29,10 +29,11 @@ def choose_logfile():
             loop_end = max_lines_on_screen
         ev3.screen.clear()
         for i in range(loop_end):
-            text_to_print = logfiles[i]#.replace(directory, ".")
+            text_to_print = logfiles[i].replace(directory, ".")
             if i == cursor_position:
                 text_to_print = "> " + text_to_print
             ev3.screen.print(text_to_print)
+            print(text_to_print)
         pressed_button = ev3.buttons.pressed()
         while not len(pressed_button) == 1:
             pressed_button = ev3.buttons.pressed()
