737c4f00ef2a04739fb78e1e3ea8b0207141894a
AutumnSpark1226 <autumnspark1226@inbox.lv>
2023-01-28 15:56:58 +0100

    2023.01.28.FIX

--- server/analysis_algorithms.py
+++ server/analysis_algorithms.py
@@ -23,7 +23,7 @@ class AnalysisThread0(threading.Thread):  # primary analysis: position objects i
         while self.keep_alive or not self.analysis_finished:
             if self.current_id <= db_operations.count_raw_data_entries():
                 self.analysis_finished = False
-                thread1.lock()
+                db_operations.lock_objects_table()
                 raw_data = db_operations.get_raw_data(self.current_id)
                 # "S1.US;S2.IR", "S1.IR;S2.US", "S1.US", "S1.IR", "S2.US", "S2.IR", "S3.US"
                 if raw_data[5].__contains__("S1"):
@@ -37,8 +37,8 @@ class AnalysisThread0(threading.Thread):  # primary analysis: position objects i
                     primary_analysis(raw_data[0], raw_data[1], raw_data[2], raw_data[3], sensor_type)
                 self.current_id += 1
             else:
+                db_operations.unlock_objects_table()
                 self.analysis_finished = True
-                thread1.unlock()
                 sleep(0.5)
         self.dead = True
 
