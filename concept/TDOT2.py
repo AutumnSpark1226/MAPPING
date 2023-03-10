e620e6f97054b65cf5966c4e9fbc54d75801f62e
AutumnSpark1226 <autumnspark1226@inbox.lv>
2023-03-10 13:44:55 +0100

    2023.03.10.0

--- concept/TDOT2.py
+++ concept/TDOT2.py
@@ -11,7 +11,7 @@ ev3 = EV3Brick()
 
 
 def demo():
-    motor_a.run_angle(200, 360)  # rotate tower
+    motor_a.run_angle(450, 360)  # rotate tower
     motor_d.run_time(200, 5000)  # drive forward
 
 
